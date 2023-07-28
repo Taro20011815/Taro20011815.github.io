1afcf487931c8108b553a6335fce8e5844f3603b
Taro20011815 <122663114+Taro20011815@users.noreply.github.com>
2023-07-28 22:11:34 +0900

    Update omikuji.js

--- js/omikuji.js
+++ js/omikuji.js
@@ -6,37 +6,37 @@ function omikuji(){
     const cont = Math.ceil(Math.random()*7);
     if(cont == 1){
         // $("#result").text("大吉");
-        $(".luck").attr('src',"../Assignment_final/img/omikuji_daikichi.png");
+        $(".luck").attr('src',"https://github.com/Taro20011815/Taro20011815.github.io/blob/main/img/omikuji_daikichi.png");
     } 
     
     else if(cont == 2){
         // $("#result").text("中吉");
-        $(".luck").attr('src',"../Assignment_final/img/omikuji_chuukichi.png");
+        $(".luck").attr('src',"https://github.com/Taro20011815/Taro20011815.github.io/blob/main/img/omikuji_chuukichi.png");
     }
 
     else if(cont == 3){
         // $("#result").text("吉");
-        $(".luck").attr('src',"../Assignment_final/img/omikuji_kichi.png");
+        $(".luck").attr('src',"https://github.com/Taro20011815/Taro20011815.github.io/blob/main/img/omikuji_kichi.png");
     }
 
     else if(cont == 4){
         // $("#result").text("小吉");
-        $(".luck").attr('src',"../Assignment_final/img/omikuji_syoukichi.png");
+        $(".luck").attr('src',"https://github.com/Taro20011815/Taro20011815.github.io/blob/main/img/omikuji_syoukichi.png");
     }
 
     else if(cont == 5){
         // $("#result").text("末吉");
-        $(".luck").attr('src',"../Assignment_final/img/omikuji_suekichi.png");
+        $(".luck").attr('src',"https://github.com/Taro20011815/Taro20011815.github.io/blob/main/img/omikuji_suekichi.png");
     }
 
     else if(cont == 6){
         // $("#result").text("凶");
-        $(".luck").attr('src',"../Assignment_final/img/omikuji_kyou.png");
+        $(".luck").attr('src',"https://github.com/Taro20011815/Taro20011815.github.io/blob/main/img/omikuji_kyou.png");
     }
 
     else if(cont == 7){
         // $("#result").text("大凶");
-        $(".luck").attr('src',"../Assignment_final/img/omikuji_daikyou.png");
+        $(".luck").attr('src',"https://github.com/Taro20011815/Taro20011815.github.io/blob/main/img/omikuji_daikyou.png");
     }
 
 }
